5a2f4a0e5168d2a84b4c1db6d8abd5f24d35c273
JullGG <166202105+JullGG@users.noreply.github.com>
2025-05-18 20:34:18 +0700

    Create ssweb.js

---
+++ api-setting/Scrape/tools/ssweb.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+
+async function ssweb(query) {
+  if (!query) throw new Error("Parameter 'query' tidak ditemukan.");
+
+  const baseURL = "https://image.thum.io/get/png/fullpage/viewportWidth/2400";
+  const finalURL = `${baseURL}/${encodeURIComponent(query)}`;
+
+  try {
+    const response = await axios.get(finalURL, {
+      responseType: "arraybuffer"
+    });
+
+    return {
+      kode: 200,
+      mime: "image/png",
+      data: response.data,
+      url: query
+    };
+  } catch (e) {
+    throw new Error(`Gagal mengambil gambar: ${e.message}`);
+  }
+}
+
+module.exports = ssweb;
