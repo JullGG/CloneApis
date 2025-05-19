3cd2be0a4a92eade0084e30432e16fbb4daf5a6c
JullGG <166202105+JullGG@users.noreply.github.com>
2025-05-19 14:23:21 +0700

    Update fb.js

--- api-setting/Scrape/downloader/fb.js
+++ api-setting/Scrape/downloader/fb.js
@@ -2,33 +2,35 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 async function facebook(url) {
-  if (!url) throw new Error("URL Facebook tidak boleh kosong!");
-
   try {
-    const form = new URLSearchParams({ url });
+    const response = await axios.post(
+      "https://www.getfvid.com/downloader",
+      new URLSearchParams({ url }).toString(),
+      {
+        headers: {
+          "content-type": "application/x-www-form-urlencoded",
+          "user-agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        },
+      }
+    );
 
-    const { data } = await axios.post("https://www.getfvid.com/downloader", form, {
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "user-agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-      },
-    });
-
-    const $ = cheerio.load(data);
-
-    const video_sd = $('a.btn-download[href*="video"]').attr("href") || null;
-    const video_hd = $('a[href*="hd"]').attr("href") || null;
-    const audio = $('a[href*=".mp3"]').attr("href") || null;
+    const $ = cheerio.load(response.data);
 
     return {
-      video_sd,
-      video_hd,
-      audio,
+      video_sd: $(
+        "body > div.page-content > div > div > div.col-lg-10.col-md-10.col-centered > div > div:nth-child(3) > div > div.col-md-4.btns-download > p:nth-child(1) > a"
+      ).attr("href"),
+      video_hd: $(
+        "body > div.page-content > div > div > div.col-lg-10.col-md-10.col-centered > div > div:nth-child(3) > div > div.col-md-4.btns-download > p:nth-child(1) > a"
+      ).attr("href"),
+      audio: $(
+        "body > div.page-content > div > div > div.col-lg-10.col-md-10.col-centered > div > div:nth-child(3) > div > div.col-md-4.btns-download > p:nth-child(2) > a"
+      ).attr("href"),
     };
-  } catch (err) {
-    throw new Error("Gagal scrap data Facebook: " + err.message);
+  } catch (error) {
+    throw error;
   }
 }
 
-module.exports = facebook;
+module.exports = { facebook };
