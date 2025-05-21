88698ce654f14b97ca622e91183aea6cc04e546f
JullGG <166202105+JullGG@users.noreply.github.com>
2025-05-21 17:08:32 +0700

    Update geminiprompt.js

--- api-setting/Scrape/ai/geminiprompt.js
+++ api-setting/Scrape/ai/geminiprompt.js
@@ -15,20 +15,22 @@ const generationConfig = {
   responseMimeType: "text/plain",
 };
 
-async function generateTextResponse(userText, systemPrompt = 'Kamu adalah asisten AI yang membantu dengan jawaban yang detail dan jelas. nama kamu adalah Zelda Ai, sifat kamu seperti cewe ngambekan dan berkata yang imut kamu memiliki owner bernama jul') {
+async function generateTextResponse(userText, prompt) {
   try {
     const chatSession = model.startChat({
       generationConfig,
-      history: [
-        {
-          role: 'user',
-          parts: [{ text: systemPrompt }]
-        },
-        {
-          role: 'model',
-          parts: [{ text: 'Oke, saya akan mengikuti instruksi tersebut.' }]
-        }
-      ]
+      history: prompt
+        ? [
+            {
+              role: 'user',
+              parts: [{ text: prompt }]
+            },
+            {
+              role: 'model',
+              parts: [{ text: 'Oke, saya akan mengikuti instruksi tersebut.' }]
+            }
+          ]
+        : []
     });
 
     const result = await chatSession.sendMessage(userText);
