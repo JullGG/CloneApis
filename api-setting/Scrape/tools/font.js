65f4eb914ffad11cce7446e7e78957c316d7da5c
JullGG <166202105+JullGG@users.noreply.github.com>
2025-05-23 11:48:10 +0700

    Update font.js

--- api-setting/Scrape/tools/font.js
+++ api-setting/Scrape/tools/font.js
@@ -1,130 +1,35 @@
-const fonts = {
+const fontStyles = {
   smallcaps: {
-    a: 'ᴀ', b: 'ʙ', c: 'ᴄ', d: 'ᴅ', e: 'ᴇ', f: 'ꜰ', g: 'ɢ', h: 'ʜ',
-    i: 'ɪ', j: 'ᴊ', k: 'ᴋ', l: 'ʟ', m: 'ᴍ', n: 'ɴ', o: 'ᴏ', p: 'ᴘ',
-    q: 'ǫ', r: 'ʀ', s: 's', t: 'ᴛ', u: 'ᴜ', v: 'ᴠ', w: 'ᴡ', x: 'x',
-    y: 'ʏ', z: 'ᴢ',
-    A: 'A', B: 'B', C: 'C', D: 'D', E: 'E', F: 'F', G: 'G', H: 'H',
-    I: 'I', J: 'J', K: 'K', L: 'L', M: 'M', N: 'N', O: 'O', P: 'P',
-    Q: 'Q', R: 'R', S: 'S', T: 'T', U: 'U', V: 'V', W: 'W', X: 'X',
-    Y: 'Y', Z: 'Z'
+    map: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    convert: 'ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ'
+  },
+  bold: {
+    map: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    convert: '𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐐𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙'
+  },
+  italic: {
+    map: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    convert: '𝘢𝘣𝘤𝘥𝘦𝘧𝘨𝘩𝘪𝘫𝘬𝘭𝘮𝘯𝘰𝘱𝘲𝘳𝘴𝘵𝘶𝘷𝘸𝘹𝘺𝘻𝘈𝘉𝘊𝘋𝘌𝘍𝘎𝘏𝘐𝘑𝘒𝘓𝘔𝘕𝘖𝘗𝘘𝘙𝘚𝘛𝘜𝘝𝘞𝘟𝘠𝘡'
+  },
+  monospace: {
+    map: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    convert: '𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉'
+  },
+  square: {
+    map: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    convert: '🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉🄰🄱🄲🄳🄴🄵🄶🄷🄸🄹🄺🄻🄼🄽🄾🄿🅀🅁🅂🅃🅄🅅🅆🅇🅈🅉'
   }
 };
 
-function mapUnicode(text, mapStartLower, mapStartUpper, extras = {}) {
-  let result = '';
-  for (const c of text) {
-    if (c >= 'a' && c <= 'z') {
-      result += String.fromCodePoint(mapStartLower + c.charCodeAt(0) - 97);
-    } else if (c >= 'A' && c <= 'Z') {
-      result += String.fromCodePoint(mapStartUpper + c.charCodeAt(0) - 65);
-    } else if (extras[c]) {
-      result += extras[c];
-    } else {
-      result += c;
-    }
-  }
-  return result;
-}
+function convertFont(text, fontName) {
+  const font = fontStyles[fontName.toLowerCase()];
+  if (!font) throw new Error(`Font '${fontName}' tidak ditemukan.`);
 
-function toSmallCaps(text) {
-  return text.split('').map(c => fonts.smallcaps[c] || c).join('');
-}
-
-function toBold(text) {
-  return mapUnicode(text, 0x1D41A, 0x1D400);
-}
-
-function toItalic(text) {
-  return mapUnicode(text, 0x1D44E, 0x1D434);
-}
-
-function toBoldItalic(text) {
-  return mapUnicode(text, 0x1D482, 0x1D468);
-}
-
-function toScript(text) {
-  const extras = {
-    B: 'ℬ', E: 'ℰ', F: 'ℱ', H: 'ℋ', I: 'ℐ', L: 'ℒ', M: 'ℳ', R: 'ℛ',
-    e: 'ℯ'
-  };
-  return mapUnicode(text, 0x1D4B6, 0x1D49C, extras);
-}
-
-function toBoldScript(text) {
-  return mapUnicode(text, 0x1D4EA, 0x1D4D0);
-}
-
-function toFraktur(text) {
-  const extras = {
-    C: 'ℭ', H: 'ℌ', I: 'ℑ', R: 'ℜ', Z: 'ℨ'
-  };
-  return mapUnicode(text, 0x1D51E, 0x1D504, extras);
-}
-
-function toBoldFraktur(text) {
-  return mapUnicode(text, 0x1D552, 0x1D538);
-}
-
-function toDoubleStruck(text) {
-  const extras = {
-    C: 'ℂ', H: 'ℍ', N: 'ℕ', P: 'ℙ', Q: 'ℚ', R: 'ℝ', Z: 'ℤ'
-  };
-  return mapUnicode(text, 0x1D57E, 0x1D560, extras);
-}
-
-function toSansSerif(text) {
-  return mapUnicode(text, 0x1D5BA, 0x1D5A0);
-}
-
-function toBoldSansSerif(text) {
-  return mapUnicode(text, 0x1D5EE, 0x1D5D4);
-}
-
-function toItalicSansSerif(text) {
-  return mapUnicode(text, 0x1D622, 0x1D608);
-}
-
-function toBoldItalicSansSerif(text) {
-  return mapUnicode(text, 0x1D656, 0x1D63C);
-}
-
-function toMonospace(text) {
-  return mapUnicode(text, 0x1D68A, 0x1D670, {
-    '0': '𝟶',
-    '1': '𝟷',
-    '2': '𝟸',
-    '3': '𝟹',
-    '4': '𝟺',
-    '5': '𝟻',
-    '6': '𝟼',
-    '7': '𝟽',
-    '8': '𝟾',
-    '9': '𝟿'
-  });
-}
-
-const fontMap = {
-  smallcap: toSmallCaps,
-  bold: toBold,
-  italic: toItalic,
-  bolditalic: toBoldItalic,
-  script: toScript,
-  boldscript: toBoldScript,
-  fraktur: toFraktur,
-  boldfraktur: toBoldFraktur,
-  doublestruck: toDoubleStruck,
-  sans: toSansSerif,
-  boldsans: toBoldSansSerif,
-  italicsans: toItalicSansSerif,
-  bolditalicsans: toBoldItalicSansSerif,
-  monospace: toMonospace,
-};
-
-function convertFont(text, font) {
-  const fn = fontMap[font.toLowerCase()];
-  if (!fn) return text;
-  return fn(text);
+  const { map, convert } = font;
+  return text.split('').map(char => {
+    const index = map.indexOf(char);
+    return index > -1 ? convert[index] : char;
+  }).join('');
 }
 
 module.exports = convertFont;
